9cd09fe42168f59135c69231e2d1e3cefe48dc53
phase <ph4sesaber@gmail.com>
2015-06-17 13:20:13 -0700

    imports

--- src/test/groovy/solar/dimensions/orbit/test/OrbitPluginTest.groovy
+++ src/test/groovy/solar/dimensions/orbit/test/OrbitPluginTest.groovy
@@ -1,8 +1,12 @@
 package solar.dimensions.orbit.test;
 
+import org.junit.*
+import org.gradle.api.*
+import org.gradle.testfixtures.*
+
 class OrbitPluginTest {
     @Test
-    public void greeterPluginAddsGreetingTaskToProject() {
+    public void pluginCreatesFiles() {
         Project project = ProjectBuilder.builder().withProjectDir(new File(testDir, "example")).build() //Build project with Example.java
         project.pluginManager.apply 'solar.dimensions.orbit' //Apply the plugin
         assertTrue(new File(project.getRootDir(), "src/main/java/solar/dimensions/api").exists()); //Does the file exist?
